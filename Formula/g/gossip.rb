7300f59ea71a18d9d064aa762d31da2a88bccdf8
git <badonyx@proton.me>
2023-09-20 18:36:24 +0000

    gossip 0.8.0 (new formula)

---
+++ Formula/g/gossip.rb
@@ -0,0 +1,19 @@
+class Gossip < Formula
+  desc "Desktop client for Nostr written in Rust"
+  homepage "https://github.com/mikedilger/gossip"
+  url "https://github.com/mikedilger/gossip.git",
+      tag:      "v0.8.0",
+      revision: "c9bb3704e94e105d7cf97de67e89ccf7f01ef69b"
+  license "MIT"
+  head "https://github.com/mikedilger/gossip.git", branch: "master"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args, "--features", "lang-cjk"
+  end
+
+  test do
+    system "#{bin}/gossip", "help"
+  end
+end
